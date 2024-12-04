423898e4731f6096dfd111b573a85b6c69ee6338
Phamviettruong11 <phamtruongviet00@gmail.com>
2024-12-04 08:56:28 +0700

    Add files via upload

---
+++ bai 5/B1TH5.py
@@ -0,0 +1,4 @@
+print("PHAM VIET TRUONG MSV:235752021610101")
+import mymath as mt
+print(mt.square(2))  
+print(mt.square(3)) 
---
+++ bai 5/B4TH5.py
@@ -0,0 +1,11 @@
+print("PHAM VIET TRUONG MSV:235752021610101")
+import  np
+
+# Tạo mảng với các giá trị từ 12 đến 38
+array = np.arange(12, 39)
+
+# Đảo ngược mảng
+reversed_array = array[::-1]
+
+print("Mảng ban đầu từ 12 đến 38:", array)
+print("Mảng sau khi đảo ngược:", reversed_array)
